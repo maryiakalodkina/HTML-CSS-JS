3019e4bd47bdd0b4a4169e8fd2b23a566d12d8a4
Maryia <mk70123n@pace.edu>
2022-04-17 22:36:04 -0400

    My first app: work in progress

---
+++ firstApp_toDoList/app.js
@@ -0,0 +1,21 @@
+const inputField = document.querySelector('.inputField');
+const btn = document.querySelector('.btn');
+const toDoContainer = document.querySelector('.toDoContainer');
+
+btn.addEventListener('click', () => {
+    const item = document.createElement('li');
+    item.innerText = inputField.value;
+    item.classList.add('toDoAdded');
+    toDoContainer.appendChild(item);
+    inputField.value = '';
+
+    item.addEventListener('click', () => {
+        item.classList.add('toDoCompleted');
+    })
+    
+    item.addEventListener('dblclick', () => {
+        toDoContainer.removeChild(item);
+    })
+})
+
+
