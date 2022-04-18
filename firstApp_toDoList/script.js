9c9c7d0016cf66099ca28dee6076a43943b1f1d2
Maryia <mk70123n@pace.edu>
2022-04-18 01:53:15 -0400

    First app: working

--- firstApp_toDoList/script.js
+++ firstApp_toDoList/script.js
@@ -1,6 +1,8 @@
 const inputField = document.querySelector('.inputField');
 const btn = document.querySelector('.btn');
 const toDoContainer = document.querySelector('.toDoContainer');
+const question = document.querySelector('.question');
+let a = 0;
 
 btn.addEventListener('click', () => {
     const item = document.createElement('li');
@@ -16,6 +18,18 @@ btn.addEventListener('click', () => {
     item.addEventListener('dblclick', () => {
         toDoContainer.removeChild(item);
     })
+})
+
+question.addEventListener('click', () => {
+    const text = document.createElement('p');
+    text.innerText = '* To cross out the task from the list, click on it once.\nTo remove the task from the list, double-click it. *\n\n Click HERE to close this message';
+    text.classList.add('toDisplayMessage'); 
+    toDoContainer.appendChild(text);
+    
+    text.addEventListener('click', () => {
+        toDoContainer.removeChild(text);
+    })
+   
 })
 
 
